2ce21e9a0801957e68165376fd270644c5871e31
Albin David C <albindavidc007@gmail.com>
2025-03-26 11:49:48 +0530

    created otp selector

---
+++ front-end/src/app/core/auth/selectors/otp.selectors.ts
@@ -0,0 +1,29 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { OTPState } from '../models/auth-state.model';
+
+export const selectOtpState = createFeatureSelector<OTPState>('otp');
+
+export const selectIsVerified = createSelector(
+  selectOtpState,
+  (state) => state.isVerified
+);
+
+export const selectOtpError = createSelector(
+  selectOtpState,
+  (state) => state.error
+);
+
+export const selectOtpLoading = createSelector(
+  selectOtpState,
+  (state) => state.isLoading
+);
+
+export const selectRemainingAttempts = createSelector(
+  selectOtpState,
+  (state) => state.remainingAttempts
+);
+
+export const selectResendAvailableIn = createSelector(
+  selectOtpState,
+  (state) => state.resendAvailableIn
+);
