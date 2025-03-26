660118d7d180f2a9b357a6da38e57cb870621a92
Albin David C <albindavidc007@gmail.com>
2025-03-26 11:32:38 +0530

    created otp reducer

---
+++ front-end/src/app/core/auth/reducer/otp.reducer.ts
@@ -0,0 +1,55 @@
+import { createReducer, on } from '@ngrx/store';
+import { OTPState } from '../models/auth-state.model';
+import { OtpActions } from '../actions/otp.actions';
+
+export const otpFeatureKey = 'otp';
+
+export const initialState: OTPState = {
+  isVerified: false,
+  error: null,
+  isLoading: false,
+  remainingAttempts: 3,
+  resendAvailableIn: 60,
+};
+
+export const reducer = createReducer(
+  initialState,
+  on(OtpActions.verifyOtp, (state) => ({
+    ...state,
+    isLoading: true,
+    error: null,
+  })),
+
+  on(OtpActions.verifyOtpSuccess, (state) => ({
+    ...state,
+    error: null,
+    isVerified: true,
+    isLoading: false,
+  })),
+
+  on(OtpActions.verifyOtpFailure, (state, { error }) => ({
+    ...state,
+    isLoading: false,
+    error,
+    remainingAttempts: initialState.remainingAttempts - 1,
+  })),
+
+  on(OtpActions.resendOtp, (state) => ({
+    ...state,
+    isLoading: true,
+    error: null,
+  })),
+
+  on(OtpActions.resendOtpSuccess, (state) => ({
+    ...state,
+    isLoading: false,
+    error: null,
+    resendAvailableIn: 60,
+  })),
+
+  on(OtpActions.resendOtpFailure, (state, { error }) => ({
+    ...state,
+    isLoading: false,
+    error,
+  }))
+);
