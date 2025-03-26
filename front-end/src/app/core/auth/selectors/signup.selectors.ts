6d41fa4192dbad27ca334d5dcd3e07e12bc84a14
Albin David C <albindavidc007@gmail.com>
2025-03-26 11:42:50 +0530

    created signup selector

---
+++ front-end/src/app/core/auth/selectors/signup.selectors.ts
@@ -0,0 +1,24 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { SignupState } from '../models/auth-state.model';
+
+export const selectSignupState = createFeatureSelector<SignupState>('signup');
+
+export const selectIsRegistered = createSelector(
+  selectSignupState,
+  (state) => state.isRegistered
+);
+
+export const selectTempUserId = createSelector(
+  selectSignupState,
+  (state) => state.tempUser?.id
+);
+
+export const selectSignupLoading = createSelector(
+  selectSignupState,
+  (state) => state.isLoading
+);
+
+export const selectSignupError = createSelector(
+  selectSignupState,
+  (state) => state.error
+);
