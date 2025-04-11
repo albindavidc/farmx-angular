b70e8fedfa663e07f1a9138dde4476b44e5021e0
Albin David C <albindavidc007@gmail.com>
2025-04-11 11:12:02 +0530

    created login store and created and updated its respective states

--- src/app/core/auth/effects/login.effects.ts
+++ src/app/core/auth/effects/login.effects.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
-
-
+import { inject, Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { LoginService } from '../services/login.service';
+import { LoginActions } from '../actions/login.actions';
+import { map, mergeMap } from 'rxjs';
+import { LoginRequest, LoginResponse } from '../models/login';
 
 @Injectable()
 export class LoginEffects {
+  private actions$ = inject(Actions);
+  private loginService = inject(LoginService);
 
-
-  constructor(private actions$: Actions) {}
+  login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(LoginActions.loadLogin),
+      mergeMap(({ request }) =>
+        this.loginService.login(request).pipe(
+          map((response: LoginResponse) => {
+            return LoginActions.loginSuccess({
+              response: {
+                user: response.user,
+                accessToken: response.accessToken,
+                refreshToken: response.refreshToken,
+              },
+            });
+          })
+        )
+      )
+    )
+  );
 }
--- src/app/core/auth/services/login.service.ts
+++ src/app/core/auth/services/login.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
 import { Observable } from 'rxjs';
 import { User } from '../models/auth-state.model';
+import { LoginRequest, LoginResponse } from '../models/login';
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +12,7 @@ export class LoginService {
   private readonly apiUrl = `${environment.apiURL}/auth`;
   constructor(private http: HttpClient) {}
 
-  login(payload: {
-    email: string;
-    password: string;
-  }): Observable<{ user: User; accessToken: string; refreshToken: string }> {
-    return this.http.post<{
-      user: User;
-      accessToken: string;
-      refreshToken: string;
-    }>(`${this.apiUrl}/login`, payload);
+  login(payload: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, payload);
   }
 }
