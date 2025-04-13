644e94a64f2563ccdfa29e7c03dd6f3fc5998bc7
Albin David C <albindavidc007@gmail.com>
2025-04-13 17:58:59 +0530

    created a wrapper component for icons because of the svg icons clash with font awsome icons.

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { faBell, faGear } from '@fortawesome/free-solid-svg-icons';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { FaWrapperComponent } from './fa-wrapper.component';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, FaWrapperComponent],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
-
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faGear, faBell);
+  }
 }
